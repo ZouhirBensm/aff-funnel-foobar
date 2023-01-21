9625da499da36b2ad6bdf288d025f70b33a342be
Zouhir Bensmaia <freelancebenz@gmail.com>
2023-01-21 17:46:13 -0500

    Overhaul, payment with option, and ability ti test in seperate directory

--- js/pp1.js
+++ js/pp1.js
@@ -9,20 +9,8 @@ function initPayPalButton() {
     },
 
     createOrder: function (data, actions) {
-      const path = window.location.pathname
-      let desciption, amount
-      switch (path) {
-        case '/linkhubwebsite.html':
-          desciption = "Link Hub Website"
-          amount = 20
-          break;
-        case '/socialmediahubwebsite.html':
-          desciption = "Social Media Hub Website"
-          amount = 15
-          break;
-        default:
-          break;
-      }
+      const desciption = "Social Media Hub Website"
+      const amount = 15
 
       return actions.order.create({
         purchase_units: [{ "description": desciption, "amount": { "currency_code": "USD", "value": amount } }]
@@ -35,36 +23,25 @@ function initPayPalButton() {
         // Full available details
         console.log('Capture result\n', orderData);
         // console.log('Capture result\n', JSON.stringify(orderData, null, 2));
-        const clientPPname = `${orderData.payer.name.given_name} ${orderData.payer.name.surname}`
-        const clientPPemail = orderData.payer.email_address
 
         // Show a success message within this page, e.g.
         const ppButtonContainer = document.getElementById('paypal-button-container');
+        const contentBlocDiv = document.getElementById('content-bloc-div');
+
         ppButtonContainer.innerHTML = '';
+        contentBlocDiv.innerHTML = '';
 
-
-        const path = window.location.pathname
-        let page_needed
-        switch (path) {
-          case '/linkhubwebsite.html':
-            page_needed = "./html/thanks_link.html"
-            break;
-          case '/socialmediahubwebsite.html':
-            page_needed = "./html/thanks_social.html"
-            break;
-          default:
-            break;
-        }
+        const page_needed = "./html/thanks_social.html"
 
         if (orderData.status == 'COMPLETED') {
 
           $.ajax({
             url: page_needed,
             success: function (data) {
-              // Get the parent element where the HTML should be inserted
-              var parent = document.getElementById("payment-processed-div");
-              // Insert the HTML into the parent element
-              parent.innerHTML = data;
+              // Get the paymentProcessedDiv element where the HTML should be inserted
+              var paymentProcessedDiv = document.getElementById("payment-processed-div");
+              // Insert the HTML into the paymentProcessedDiv element
+              paymentProcessedDiv.innerHTML = data;
               return
             }
           })
@@ -85,8 +62,6 @@ function initPayPalButton() {
 
             return
           })
-
-          console.log("Done ajax");
         } else {
           $("#payment-processed-div").load("./html/payment_error.html");
         }
