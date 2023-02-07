0bd4752bc89e0437791717ddf4c84b246ebc959b
Zouhir Bensmaia <freelancebenz@gmail.com>
2023-02-06 21:31:34 -0500

    I should now be able to track clicks

--- js/track-clicks.js
+++ js/track-clicks.js
@@ -1,8 +1,9 @@
-
-// Track user clicks
+// TODO adjust font size level depending on zoom level
+// var zoomLevel = window.devicePixelRatio;
+// console.log("Zoom level: " + zoomLevel);
 
 execDeterminator();
-
+// Track user clicks
 function execDeterminator() {
 
   console.log("execDeterminator")
@@ -13,31 +14,40 @@ function execDeterminator() {
 
   var referrer_domain = new URL(referrer).hostname;
 
-  // URL got clicked by external domain
-  if (referrer_domain === document.location.hostname) return
-
-  return iterate(referrer_domain)
+  // URL referrer is internal domain
+  if (referrer_domain === document.location.hostname) {
+    if(window.location.pathname !== '/') return iterate()
+    return
+  } else {
+    // URL got clicked by external domain
+    return iterate()
+  }
+  
 }
 
 
 
 
 
-async function iterate(referrer_domain = undefined) {
+async function iterate() {
 
     console.log("iterate")
+
     // TODO Add refferer tracking later
     // ${referrer_domain? `?referrer_domain=${referrer_domain}`: null}
+
     // let url = `http://localhost:8888/HTTP/Foobar/track-clicks/foobaropom.php`
-    let url = 'https://webdevelopercanada.website/HTTP/Foobar/track-clicks/foobaropom.php'
+    let url = `https://webdevelopercanada.website/HTTP/Foobar/track-clicks/foobaropom.php`
 
 
-   $.ajax({
+  $.ajax({
     url: url,
-    success: function (response) {
+    data: {body: JSON.stringify({pathname: window.location.pathname})},
+    type: 'POST',
+    success: function(response) {
       console.log("track-clicks: registerClickCall()->", response)
       return
-    }
-  })
+    },
+ });
 }
 
